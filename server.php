d59e9ea0c4cc8c02997fd3a80d941d3996baeb75
gaby <gabyara294@gmail.com>
2019-06-22 13:59:59 -0400

    Primer commit

---
+++ server.php
@@ -0,0 +1,46 @@
+<?php
+require_once('lib/nusoap.php');
+
+// Create the server instance
+$server = new soap_server();
+$server->configureWSDL('server', 'urn:server');
+
+$server->wsdl->schemaTargetNamespace = 'urn:server';
+
+// SOAP complex type return type (an array/struct)
+$server->wsdl->addComplexType(
+   'Person',
+   'complexType',
+   'struct',
+   'all',
+   '',
+   array('id_user' => array('name' => 'id_user',
+         'type' => 'xsd:int'))
+);
+
+// Register the "Monitorear" method to expose it
+$server->register('Monitorear',
+         array('name' => 'xsd:string','date' => 'xsd:string'),   // parameter
+         array('return' => 'xsd:string','Disponibilidad' => 'xsd:string'),     // output
+         'urn:server',                        // namespace
+         'urn:server#NonitorearServer',            // soapaction
+         'rpc',                               // style
+         'encoded',                           // use
+         'Informa disponibilidad');                   // description
+
+// Implement the "Monitorear" method as a PHP function
+function Monitorear($name,$date) {
+
+$rnd = rand(0,100);
+$Disponibilidad= $rnd/100;
+
+	
+   return array('return'=>$name,'Disponibilidad'=> $Disponibilidad);
+
+}
+
+// Use the request to invoke the service
+$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA)
+   ? $HTTP_RAW_POST_DATA : '';
+$server->service($HTTP_RAW_POST_DATA);
+?>
